bf37442c290d62bca119540b044fc5aa322ce33f
Marcos Vinicius <marcos.brasilino@fatec.sp.gov.br>
2017-12-03 15:29:22 -0200

    Git ignore + exercícios

---
+++ 1003.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main()
+{
+	int soma, a, b;
+
+	std::cin >> a;
+	std::cin >> b;
+	soma = a + b ;
+
+	std::cout << "SOMA = " << soma << std::endl;
+	return 0;
+}
---
+++ 1004.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	int prod, n1, n2;
+
+	std::cin >> n1;
+	std::cin >> n2;
+	prod = n1 * n2;
+	std::cout << "PROD = " << prod << std::endl;
+	return 0;
+}
